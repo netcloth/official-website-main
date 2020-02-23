695c5c5357ad55a0a70c3629a6d27f1710bbd79a
lihn1987 <lihn1011@163.com>
2020-02-24 00:30:38 +0800

    v0.1.8

--- src/components/header.vue
+++ src/components/header.vue
@@ -38,7 +38,7 @@
 
   @Component
   export default class Header extends Vue {
-    @Prop({ default: 'https://cdn.jsdelivr.net/gh/netcloth/official-website-main@v0.1.7/dist/images/header/nav-logo.png' }) readonly imgSrc!: string;
+    @Prop({ default: 'https://cdn.jsdelivr.net/gh/netcloth/official-website-main@v0.1.8//dist/images/header/nav-logo.png' }) readonly imgSrc!: string;
     @Prop({ default: '#ffffff' }) readonly Pcolor!: string;
     // methods
     private ChangLang(value: string): void {
