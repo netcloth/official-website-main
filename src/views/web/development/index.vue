e265b95d66439912cdd899bc10844f0438f788cd
lihn1987 <lihn1011@163.com>
2020-02-24 11:42:14 +0800

    v0.1.9

--- src/views/web/development/index.vue
+++ src/views/web/development/index.vue
@@ -7,7 +7,7 @@
  -->
 <template>
   <div class="development">
-    <Header imgSrc='https://cdn.jsdelivr.net/gh/netcloth/official-website-main@v0.1.8//dist/images/development/logo-red.png' Pcolor='#17213C' />
+    <Header imgSrc='https://cdn.jsdelivr.net/gh/netcloth/official-website-main@v0.1.9/dist/images/development/logo-red.png' Pcolor='#17213C' />
     <devHome />
     <div class="footer">
       <Footer />
